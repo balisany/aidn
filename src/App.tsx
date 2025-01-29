07d3c602fb6e94072c7ff0e47560ddd92f264c7e
Dlzar Balisany <dlzar.balisany@consid.no>
2025-01-29 21:54:37 +0100

    setup simple fetch data with API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import useSWR from "swr";
 import "./App.css";
+import axios from "axios";
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 function App() {
+  const { data, isLoading, error } = useSWR(
+    "https://api.thedogapi.com/v1/breeds",
+    fetcher
+  );
+
   return (
     <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
       <h1 className="text-3xl text-aidn-blue font-bold underline">
-        Hello world!
+        Dog Breeds
       </h1>
+      <ul>
+        {data?.map((breed: { id: number; name: string }) => (
+          <li key={breed.id}>{breed.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
