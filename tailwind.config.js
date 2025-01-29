bf1081995e8a0205ccf272f1686686ea5fa354b5
Dlzar Balisany <dlzar.balisany@consid.no>
2025-01-29 21:48:03 +0100

    add custom color palette and update heading styles in App component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'aidn': {
+          green: 'var(--aidn-green)',
+          blue: 'var(--aidn-blue)',
+          yellow: 'var(--aidn-yellow)',
+        },
+      },
+    },
   },
   plugins: [],
 }
