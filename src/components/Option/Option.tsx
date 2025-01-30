876f19f52c7e4932fe182b4b694568e1137f46fc
Dlzar Balisany <dlzar.balisany@consid.no>
2025-01-30 11:34:06 +0100

    feat: add random badge colors to Option component for enhanced visual distinction

--- src/components/Option/Option.tsx
+++ src/components/Option/Option.tsx
@@ -8,13 +8,16 @@ type OptionProps = {
 
 function Option({ data, selected, onSelect }: OptionProps) {
   const isSelected = selected === data.id;
+  const badgeColors = ["aidn-green", "aidn-blue", "aidn-yellow"];
+  const randomColor =
+    badgeColors[Math.floor(Math.random() * badgeColors.length)];
   return (
     <li
       className={`${
         isSelected && "bg-aidn-green"
       } flex items-center gap-4 px-4 hover:bg-gray-200`}
     >
-      <div className="w-4 h-4 rounded-full bg-gray-800"></div>
+      <div className={`w-4 h-4 rounded-full bg-${randomColor}`}></div>
       <button
         className="w-full p-2 text-left"
         onClick={() => onSelect(data.id)}
